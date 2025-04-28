2c070ea02b4f2e6863682ffa3c3703a95026282a
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-28 08:15:16 +0100

    Task fix form solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+const inputWrapper = document.querySelectorAll('.field');
+const inputs = document.querySelectorAll('.field input');
+
+const inputNames = [];
+
+for (let i = 0; i < inputs.length; i++) {
+  inputNames.push(inputs[i].getAttribute('name'));
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', inputNames[i].id);
+
+  const labelText = inputNames[i];
+
+  label.innerText = labelText;
+  inputWrapper[i].appendChild(label);
+}
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].setAttribute('placeholder', capitalize(inputs[i].name, true));
+}
